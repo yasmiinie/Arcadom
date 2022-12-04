47c08dc898bbc23fbef113016eff99b578ef7fde
yasmiinie <ly_zaidi@esi.dz>
2022-12-05 00:05:41 +0100

    home page

---
+++ main.js
@@ -0,0 +1,53 @@
+
+/*font-family: 'Bitter', serif;
+
+font-family: 'Rubik', sans-serif;
+font-family: 'debug-free-trial', sans-serif;
+
+
+
+body{
+    margin: 0;
+    font-family: 'Inter', sans-serif;
+}
+
+
+
+
+
+.logo{
+   content: url('images/logo\ and\ text.png');
+}
+
+
+
+
+
+nav .logo{
+
+border-radius: 29.6122px;
+width: 134.52px;
+height: 109.99px;
+
+}
+
+
+
+
+
+
+.hero-section{
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.hero-section .left h2{
+    font-weight: 400;
+    font-size: 60px;
+    line-height: 54px;
+    color: #FFFFFF;
+    font-family: 'DEBUG FREE TRIAL',sans-serif;
+}*/
